c66ede956dd08c01c1f460597e16196fb58c25e6
unknown <semion.tiukov@gmail.com>
2023-05-19 09:15:38 +0300

    test js commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+
+console.log(number);
+
+const arr = [1, 2, 3];
+console.log(arr[1]);
+
+const obj = {
+    Anna: 500,
+    Alice: 800
+};
+
+console.log(obj['Anna']);
